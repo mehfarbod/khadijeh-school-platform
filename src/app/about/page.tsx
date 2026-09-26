f7ea435150fc55da650f11575b508e0f39c5f364
mehfarbod <mehdifarbod.dev@gmail.com>
2026-09-26 17:59:58 +0330

    feat: load about page content from database

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,18 @@ import Header from "@/components/layout/Header";
 import CoursesFooter from "@/components/courses/CoursesFooter";
 import AboutHero from "@/components/about/AboutHero";
 import SchoolIntro from "@/components/about/SchoolIntro";
+import { prisma } from "@/lib/prisma";
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const about = await prisma.aboutPage.findUnique({ where: { id: "school-about" } });
   return (
     <>
       <Header />
 
       <main>
-        <AboutHero />
+        <AboutHero data={about} />
 
-        <SchoolIntro />
+        <SchoolIntro data={about} />
       </main>
 
       <CoursesFooter />
